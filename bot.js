b8fb86d9ccac3126ca1bc74d2c2354dab060f1c4
shankapotomus <vrishank.subramani@gmail.com>
2016-03-30 13:37:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i; botRegexSalt = /^\/salt/; botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
-      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/; botRegexS = /^\/schedule; 
+      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -19,11 +19,6 @@ function respond() {
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://daddyleagues.com/1738rmlar/team/"+request.text.substring(5,8)+"/depthchart");
-    this.res.end();
-  } 
-    else if(request.text && botRegexS.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/1738rmlar/schedules");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
