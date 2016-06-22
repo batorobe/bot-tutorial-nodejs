18cbfdc5d3dea4bf62205b597b31a58ee09683a3
jhodge521 <jhodge521@gmail.com>
2016-06-22 15:17:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ function respond() {
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/; botRegexOW = /^\/overwatch/; 
       botRegexDenk = /^\.denk/;
+      botRegexGamb = /^\.potd/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -19,6 +20,12 @@ function respond() {
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage(cool());
+    this.res.end();
+  } 
+  
+  if(request.text && botRegexGamb.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Stop gambling you degenerate fuck.");
     this.res.end();
   } 
   
