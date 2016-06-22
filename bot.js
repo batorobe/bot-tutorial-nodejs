3b57ca533cc2667ef0d80a471a12d4cdb4dd40dc
jhodge521 <jhodge521@gmail.com>
2016-06-22 16:14:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,8 @@ function respond() {
       botRegexSiege = /^\/siege/; botRegexOW = /^\/overwatch/; 
       botRegexDenk = /^\.denk/;
       botRegexGamb = /^\.potd/;
-      botRegexWil = /^\.willy/;
+      botRegexMaga = /(maga)|(MAGA)|(Maga)|(kek)|(Kek)|(top kek)|(Top kek)|(Top Kek)/;
+      botRegexWil = /(willy)|(Willy)|(Willard)|(William)/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -23,6 +24,12 @@ function respond() {
     postMessage(cool());
     this.res.end();
   } 
+  
+  else if(request.text && botRegexMaga.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/vVjHJUJ.png");
+    this.res.end();
+  }
   
   else if(request.text && botRegexWil.test(request.text)) {
     this.res.writeHead(200);
