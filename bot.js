e4382677c1bc47ff7f66f1e919e072b6c0fb7d7f
jhodge521 <jhodge521@gmail.com>
2016-06-23 09:22:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,13 +10,13 @@ function respond() {
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexDenk = /^\.denk/;
       botRegexGamb = /^\.potd/;
-      botRegexMaga = /(maga)|(MAGA)|(Maga)|(kek)|(Kek)|(top kek)|(Top kek)|(Top Kek)/;
+  //    botRegexMaga = /(maga)|(MAGA)|(Maga)|(kek)|(Kek)|(top kek)|(Top kek)|(Top Kek)/;
       botRegexWil = /^\/.willy/;
       
       //images for maga call
-      maga1 = 'https://i.sli.mg/E3BbKK.jpg'; maga2 = 'http://trumpimages.com/TqfJA.jpg'
-      maga3 = 'http://trumpimages.com/YECTD.jpg'; maga4 = 'https://i.redditmedia.com/3ajTo756OzvESMhS1uvrz4GfRgIaD-IjFrua8uHZasc.jpg?w=349&s=54ca7e69b999bd9d60ce33610b38cfee';
-      maga5 = 'https://i.sli.mg/l2knjY.jpg'
+  //    maga1 = 'https://i.sli.mg/E3BbKK.jpg'; maga2 = 'http://trumpimages.com/TqfJA.jpg'
+  //    maga3 = 'http://trumpimages.com/YECTD.jpg'; maga4 = 'https://i.redditmedia.com/3ajTo756OzvESMhS1uvrz4GfRgIaD-IjFrua8uHZasc.jpg?w=349&s=54ca7e69b999bd9d60ce33610b38cfee';
+   //   maga5 = 'https://i.sli.mg/l2knjY.jpg'
  
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
