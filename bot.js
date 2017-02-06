9b201eb68a80cdecf502d85b2169947f37cdb24c
abassham <abbassham@gmail.com>
2017-02-06 12:11:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,49 +3,18 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
-function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegexArena = /(A|a)arena cutoff/;botRegexTitan = /(T|t)itan/;
-      botRegexBK=/(B|b)oss (K|k)iller/;botRegexBI = /(B|b)itch/; 
-  if(request.text && botRegexArena.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("https://docs.google.com/spreadsheets/d/1sJtSVnjhhRNxpiuMR5uXrsTlrsXMjp9TNO7JHDXhtsk/htmlview?pli=1");
-    this.res.end();
-  }
-  else if(request.text && botRegexTitan.test(request.text)) {
+var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /(T|t)itan/;botRegexB = /(B|b)ot/;
+  if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage("#ishebackyet?");
     this.res.end();
-  } 
-  else if(request.text && botRegexBK.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("#bosskillah");
-    this.res.end();
-  } 
-  else if(request.text && botRegexBI.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage(/#thatbih/);
-    this.res.end();
   }
-  
-  else if(request.text && botRegexDaf.test(request.text)) {
+  else if(request.text && botRegexB.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://i3.kym-cdn.com/photos/images/facebook/000/787/356/d6f.jpg");
+    postMessage("here");
     this.res.end();
   } 
-  else if(request.text && botRegexAbassbot.test(request.text)) {
-    var phraseArray = [ "here",
-                       "yes",
-                       "?" ];
-    function chooseRandom(myArray) {
-    return myArray[Math.floor(Math.random() * myArray.length)];
-}
-    var phrase = chooseRandom(phraseArray);
-}
-    this.res.writeHead(200);
-    postMessage(phraseArray);
-    this.res.end();
-}
   else {
     console.log("don't care");
     this.res.writeHead(200);
